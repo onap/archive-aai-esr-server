03cb28b3dfabbf7c49c645dde29a74fd5e60bbf8
lizi00164331 <li.zi30@zte.com.cn>
2017-09-07 15:10:59 +0800

    Define AAI Vim register related API
    Define AAI Vim register related API.Remove original MSB related class,this will realize by using java-sdk. Define external system bean related to AAI.
    
    Change-Id: I1e99ea95e4e1e458787e3665979a1afeb4c11750
    Issue-ID: AAI-263
    Signed-off-by: lizi00164331 <li.zi30@zte.com.cn>

---
+++ esr-core/esr-mgr/src/main/java/org/onap/aai/esr/externalservice/aai/AaiCommon.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.aai.esr.externalservice.aai;
+
+import java.io.IOException;
+import java.io.StringReader;
+import com.sun.jersey.api.client.ClientResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class AaiCommon {
+
+  private static String AAI_AUTHENTICATION_USER = "AAI";
+  private static String AAI_AUTHENTICATION_PAASWORD = "AAI";
+  private static String RESOURCE_VERSION_PARAM = "resource-version";
+  
+//  public String getAuthenticationCredentials() {
+//    String usernameAndPassword = AAI_AUTHENTICATION_USER + ":"
+//        + AAI_AUTHENTICATION_PAASWORD;
+//    return "Basic " + java.util.Base64.getEncoder().encodeToString("AAI:AAI".getBytes());
+//  }
+  
+  public String getResourceVersion(ClientResponse response)
+      throws ParserConfigurationException, SAXException, IOException {
+    String respData = response.getEntity(String.class);
+
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    InputSource is = new InputSource(new StringReader(respData));
+    Document doc = builder.parse(is);
+
+    NodeList nodeList = doc.getDocumentElement().getChildNodes();
+    for (int i = 0; i < nodeList.getLength(); i++) {
+      Node currentNode = nodeList.item(i);
+      if (currentNode.getNodeName().equals(RESOURCE_VERSION_PARAM)) {
+        return currentNode.getTextContent();
+      }
+    }
+
+    return null;
+  }
+}
