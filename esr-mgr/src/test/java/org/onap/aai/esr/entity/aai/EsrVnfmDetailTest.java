c401a51179f66ad22b462320d2b680fc6e564fdd
lizi00164331 <li.zi30@zte.com.cn>
2017-09-20 16:48:46 +0800

    Add unit test for VNFM bean class.
    Change-Id: Icdb9009bc343d1b931126df927684e8d8cb4cc3b
    Issue-ID: AAI-351
    Signed-off-by: lizi00164331 <li.zi30@zte.com.cn>

---
+++ esr-mgr/src/test/java/org/onap/aai/esr/entity/aai/EsrVnfmDetailTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.aai.esr.entity.aai;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class EsrVnfmDetailTest {
+  @Test
+  public void getterAndSetter4vnfmId(){
+      final String vnfmId = "vnfmId-test";
+      EsrVnfmDetail esrVnfm = new EsrVnfmDetail();
+      esrVnfm.setVnfmId(vnfmId);
+      assertEquals(esrVnfm.getVnfmId(), vnfmId);
+  }
+  
+  @Test
+  public void getterAndSetter4resourceVersion(){
+      final String resourceVersion = "resourceVersion-test";
+      EsrVnfmDetail esrVnfm = new EsrVnfmDetail();
+      esrVnfm.setResourceVersion(resourceVersion);
+      assertEquals(esrVnfm.getResourceVersion(), resourceVersion);
+  }
+  
+  @Test
+  public void getterAndSetter4vimId(){
+      final String vimId = "vimId-test";
+      EsrVnfmDetail esrVnfm = new EsrVnfmDetail();
+      esrVnfm.setVimId(vimId);
+      assertEquals(esrVnfm.getVimId(), vimId);
+  }
+  
+  @Test
+  public void getterAndSetter4certificateUrl(){
+      final String certificateUrl = "certificateUrl-test";
+      EsrVnfmDetail esrVnfm = new EsrVnfmDetail();
+      esrVnfm.setCertificateUrl(certificateUrl);
+      assertEquals(esrVnfm.getCertificateUrl(), certificateUrl);
+  }
+  
+  @Test
+  public void getterAndSetter4esrSystemInfoList(){
+    final EsrSystemInfoList esrSystemInfoList = new EsrSystemInfoList();
+    ArrayList<EsrSystemInfo> esrSystemInfo = new ArrayList<EsrSystemInfo>();
+    EsrSystemInfo esrSystemInfoObj = new EsrSystemInfo();
+    esrSystemInfoObj.setEsrSystemInfoId("123");
+    esrSystemInfo.add(esrSystemInfoObj );
+    esrSystemInfoList.setEsrSystemInfo(esrSystemInfo);
+    EsrVnfmDetail esrVnfmDetail = new EsrVnfmDetail();
+    esrVnfmDetail.setEsrSystemInfoList(esrSystemInfoList);
+    assertEquals(esrVnfmDetail.getEsrSystemInfoList(), esrSystemInfoList);
+  }
+}
---
+++ esr-mgr/src/test/java/org/onap/aai/esr/entity/aai/EsrVnfmListTest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.aai.esr.entity.aai;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class EsrVnfmListTest {
+  @Test
+  public void getterAndSetter4EsrVnfmList() {
+    EsrVnfmList esrVnfmList = new EsrVnfmList();
+    ArrayList<EsrVnfm> esrVnfms = new ArrayList<EsrVnfm>();
+    EsrVnfm esrVnfm = new EsrVnfm();
+    esrVnfm.setVnfmId("fadasf");
+    esrVnfms.add(esrVnfm);
+    esrVnfmList.setEsrVnfm(esrVnfms);
+    assertEquals(esrVnfmList.getEsrVnfm(), esrVnfms);
+  }
+}
