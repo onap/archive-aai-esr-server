210726de49fe8381ce38521b8896d8a9532f3076
lizi00164331 <li.zi30@zte.com.cn>
2017-09-20 15:37:13 +0800

    Add unit test for esr ems bean.
    Change-Id: Ifc15f2abbf19e5ba4ef5b9eb36c5bf709fadcf76
    Issue-ID: AAI-344
    Signed-off-by: lizi00164331 <li.zi30@zte.com.cn>

---
+++ esr-mgr/src/test/java/org/onap/aai/esr/entity/aai/EsrEmsDetailTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2017 ZTE Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.aai.esr.entity.aai;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class EsrEmsDetailTest {
+
+  @Test
+  public void getterAndSetter4emsId(){
+      final String emsId = "emsId-test";
+      EsrEmsDetail esrEmsDetail = new EsrEmsDetail();
+      esrEmsDetail.setEmsId(emsId);
+      assertEquals(esrEmsDetail.getEmsId(), emsId);
+  }
+  
+  @Test
+  public void getterAndSetter4resourceVersion(){
+      final String resourceVersion = "resourceVersion-test";
+      EsrEmsDetail esrEmsDetail = new EsrEmsDetail();
+      esrEmsDetail.setResourceVersion(resourceVersion);
+      assertEquals(esrEmsDetail.getResourceVersion(), resourceVersion);
+  }
+  
+  @Test
+  public void getterAndSetter4esrSystemInfoList(){
+    final EsrSystemInfoList esrSystemInfoList = new EsrSystemInfoList();
+    ArrayList<EsrSystemInfo> esrSystemInfo = new ArrayList<EsrSystemInfo>();
+    EsrSystemInfo esrSystemInfoObj = new EsrSystemInfo();
+    esrSystemInfoObj.setEsrSystemInfoId("123");
+    esrSystemInfo.add(esrSystemInfoObj );
+    esrSystemInfoList.setEsrSystemInfo(esrSystemInfo);
+    EsrEmsDetail cloudRegion = new EsrEmsDetail();
+    cloudRegion.setEsrSystemInfoList(esrSystemInfoList);
+    assertEquals(cloudRegion.getEsrSystemInfoList(), esrSystemInfoList);
+  }
+}
