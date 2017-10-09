5643f9a5d8a9463e6de6bbfe4eaf441b9351af01
lizi <li.zi30@zte.com.cn>
2017-10-09 19:01:31 +0800

    Fix the extsys.yml config.
    Change-Id: Ie6835e9c3c1abb1a05e6996f8c3c82f92b7a5a13
    Issue-ID: AAI-420
    Signed-off-by: lizi <li.zi30@zte.com.cn>

--- standalone/src/main/assembly/docker/instance_config.sh
+++ standalone/src/main/assembly/docker/instance_config.sh
@@ -3,7 +3,7 @@
 MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
 MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 
-sed -i "s|msbDiscoveryIp.*|msbDiscoveryIp = '$MSB_IP'|" /home/esr/conf/extsys.yml
-sed -i "s|msbServerAddr.*|msbServerAddr = 'http://$MSB_IP:80'|" /home/esr/conf/extsys.yml
+sed -i "s|msbDiscoveryIp.*|msbDiscoveryIp: $MSB_IP|" /home/esr/conf/extsys.yml
+sed -i "s|msbServerAddr.*|msbServerAddr: http://$MSB_IP:80|" /home/esr/conf/extsys.yml
 
 cat /home/esr/conf/extsys.yml
