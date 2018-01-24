4d2e7d006e7f3a166bb883b7a3d8ba223d0f8625
Swapnali Pode <SP00501638@techmahindra.com>
2018-01-24 12:23:22 +0530

    Move string literal to left side of comparison
    Move true string literal on the left side of this string comparison
    ExtsysApp.java:L56
    
    Sonar Link:
    https://sonar.onap.org/component_issues/index?id=org.onap.aai.esr-server%3Aaai-esr-server#resolved=false|assignees=swapnalipode
    Location:
    src/main/java/org/onap/aai/esr/ExtsysApp.java
    
    Change-Id: I82e43ca7a68322c6d409ecc113932e289721c374
    Issue-ID: AAI-691
    Signed-off-by: Swapnali Pode <SP00501638@techmahindra.com>

--- esr-mgr/src/main/java/org/onap/aai/esr/ExtsysApp.java
+++ esr-mgr/src/main/java/org/onap/aai/esr/ExtsysApp.java
@@ -53,7 +53,7 @@ public class ExtsysApp extends Application<ExtsysAppConfiguration> {
         environment.jersey().register(new VimManager());
         environment.jersey().register(new VnfmManager());
         environment.jersey().register(new ServiceTest());
-        if (configuration.getRegistByHand().equals("true")) {
+        if ("true".equals(configuration.getRegistByHand())) {
             String MSB_IP = configuration.getMsbDiscoveryIp();
             Integer MSB_Port = Integer.valueOf(configuration.getMsbDiscoveryPort());
             MSBServiceClient msbClient = new MSBServiceClient(MSB_IP, MSB_Port);
