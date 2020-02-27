9b2ba77ae53ead7c68937a2221d70396a5b27512
LaMont, William(wl2432) <wl2432@att.com>
2020-02-27 12:31:33 -0500

    add port to extsys for ESR connection
    Issue-ID: AAI-2734
    Change-Id: Iba1849e0d73722eb0cac0598245d2b216aaa4381
    Signed-off-by: LaMont, William(wl2432) <wl2432@att.com>

--- standalone/src/main/assembly/docker/instance_config.sh
+++ standalone/src/main/assembly/docker/instance_config.sh
@@ -4,6 +4,6 @@ MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
 MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 
 sed -i "s|msbDiscoveryIp.*|msbDiscoveryIp: $MSB_IP|" /home/esr/conf/extsys.yml
-sed -i "s|msbServerAddr.*|msbServerAddr: http://$MSB_IP|" /home/esr/conf/extsys.yml
+sed -i "s|msbServerAddr.*|msbServerAddr: http://$MSB_IP:$MSB_PORT|" /home/esr/conf/extsys.yml
 
 cat /home/esr/conf/extsys.yml
