49b16ddb15c9fc75b3809d0c8ab6b777a4ec555f
HePeng <he.peng6@zte.com.cn>
2020-05-20 15:56:19 +0800

    Remove MSB from Container
    Issue-ID: AAI-2906
    Change-Id: I80aef72c43dd357aabb6f6c118ef38e796f4f8c4
    Signed-off-by: HePeng <he.peng6@zte.com.cn>

--- standalone/src/main/assembly/docker/docker-entrypoint.sh
+++ standalone/src/main/assembly/docker/docker-entrypoint.sh
@@ -7,16 +7,16 @@ echo "SERVICE_IP=$SERVICE_IP"
 
 if [ -z "$MSB_ADDR" ]; then
     echo "Missing required variable MSB_ADDR: Microservices Service Bus address <ip>:<port>"
-    exit 1
+#   exit 1
 fi
 echo "MSB_ADDR=$MSB_ADDR"
 
 # Wait for MSB initialization
-echo "Wait for MSB initialization"
-for i in {1..5}; do
-    curl -sS -m 1 $MSB_ADDR > /dev/null && break
-    sleep $i
-done
+#echo "Wait for MSB initialization"
+#for i in {1..5}; do
+#    curl -sS -m 1 $MSB_ADDR > /dev/null && break
+#    sleep $i
+#done
 
 # Configure service based on docker environment variables
 /home/esr/docker/instance_config.sh
